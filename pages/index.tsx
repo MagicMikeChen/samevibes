223b18ce0912ff2acf952ab32e89d61d1ed856d9
MikeChen-Work <>
2021-12-16 17:33:55 +0800

    feat: Add  "@tailwindcss/forms": "^0.4.0"

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,19 +12,52 @@ const Home: NextPage = () => {
       </Head>
       <div className="container mx-auto">
         <Navbar></Navbar>
-        <div className="flex mt-16" >
-          <div className="h-80 w-full bg-gray-900 bg-opacity-30 rounded-xl backdrop-blur-2xl mx-4 py-4 px-4 cs-border-zinc-t-600">
+        <div className="flex mt-16">
+          <div className="h-full w-full bg-gray-900 bg-opacity-30 rounded-xl backdrop-blur-2xl mx-4 py-4 px-4 cs-border-zinc-t-600">
             <div className="relative mt-8 mx-2">
-              <input id="username" name="username" type="text" className="peer h-10 w-full border-b-2 bg-transparent border-gray-400 text-white placeholder-transparent focus:outline-none focus:border-grey-700" placeholder="name" />
-              <label className="absolute left-0 -top-3.5 text-sm transition-all peer-placeholder-shown:text-base text-white peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-white peer-focus:text-sm">User name</label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                className="peer h-10 w-full border-b-2 bg-transparent border-gray-500 text-white placeholder-transparent focus:outline-none focus:border-grey-500"
+                placeholder="name"
+              />
+              <label className="absolute left-0 -top-3.5 text-sm transition-all peer-placeholder-shown:text-base text-white peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-white peer-focus:text-sm">
+                User name
+              </label>
             </div>
             <div className="relative mt-8 mx-2">
-              <input id="email" name="email" type="text" className="peer h-10 w-full border-b-2 bg-transparent border-gray-400 text-white placeholder-transparent focus:outline-none focus:border-grey-500" placeholder="john@doe.com" />
-              <label className="absolute left-0 -top-3.5 text-sm transition-all peer-placeholder-shown:text-base text-white peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-white peer-focus:text-sm">Email address</label>
+              <input
+                id="email"
+                name="email"
+                type="text"
+                className="peer h-10 w-full border-b-2 bg-transparent border-gray-500 text-white placeholder-transparent focus:outline-none focus:border-grey-500"
+                placeholder="john@doe.com"
+              />
+              <label className="absolute left-0 -top-3.5 text-sm transition-all peer-placeholder-shown:text-base text-white peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-white peer-focus:text-sm">
+                Email address
+              </label>
             </div>
             <div className="base text-white mt-8 mx-2">Gender</div>
             <div className="cs-border-btn-t-100 rounded-xl text-lg font-medium text-gray-400 py-2 px-2 mt-2 mx-2">
               <div>Choose</div>
+            </div>
+            <div className='mt-4 ml-2'>
+              <label className="text-sm font-medium text-white ">
+                Company size
+              </label>
+              <div className="mt-2">
+                <select
+                  name="company-size"
+                  id="company-size"
+                  className="cs-border-btn-t-100 rounded-xl text-lg font-medium text-gray-400 bg-transparent"
+                >
+                  <option value="">Please select</option>
+                  <option value="small">1 to 10 employees</option>
+                  <option value="medium">11 to 50 employees</option>
+                  <option value="large">50+ employees</option>
+                </select>
+              </div>
             </div>
           </div>
         </div>
