b93c2ecdb344d9b4e7728dee4f6fd020544065f0
MikeChen-Work <>
2022-01-03 16:32:16 +0800

    feat: Add audio player

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,11 @@
 import type { NextComponentType } from 'next';
 import AudioPlayer from './AudioPlayer';
-import AudioPlayer2 from './AudioPlayer2';
 
 const Layout: NextComponentType = ({ children }) => {
   return (
     <div>
       <div className='fixed bottom-0 left-0 z-20 w-full'>
-        <AudioPlayer2/>
+        <AudioPlayer/>
       </div>
       <main>{children}</main>
     </div>
