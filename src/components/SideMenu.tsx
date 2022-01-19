a8b94c8d7f0dbdd7f8d1739b4cf459d4478b9777
MikeChen-Work <>
2022-01-19 10:52:04 +0800

    fix: Fixed sidemenu overlay close modal

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -73,7 +73,7 @@ export const SideMenu = () => {
           </motion.div>
         </motion.div>
         {isOpen && (
-          <motion.div className="absolute top-0 left-0 w-screen h-screen z-10 bg-black bg-opacity-10"></motion.div>
+          <motion.div className="absolute top-0 left-0 w-screen h-screen z-10 bg-black bg-opacity-10" onClick={() => toggleOpen()}></motion.div>
         )}
         <MenuToggleButton toggle={() => toggleOpen()} />
     </motion.div>
